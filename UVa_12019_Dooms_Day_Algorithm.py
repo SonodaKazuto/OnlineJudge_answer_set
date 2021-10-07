fae658697599e881b19937e8f56d6c561c296f1b
SonodaKazuto <86616409+SonodaKazuto@users.noreply.github.com>
2021-10-07 08:17:05 +0800

    UVa_12019_Dooms_Day_Algorithm(reformat)

--- UVa_12019_Dooms_Day_Algorithm.py
+++ UVa_12019_Dooms_Day_Algorithm.py
@@ -1,4 +1,4 @@
-def weekdayChecker(_month_, _day_):
+def weekdaychecker(_month_, _day_):
     m = [i for i in range(1, 13)]
     d = [10, 21, 7, 4, 9, 6, 11, 8, 5, 10, 7, 12]
     dooms = dict(zip(m, d))
@@ -6,15 +6,15 @@ def weekdayChecker(_month_, _day_):
 
     weekday = 1
     tmp = _day_ - dooms[_month_]
-    if tmp >= 0 :
+    if tmp >= 0:
         weekday += tmp % 7
-    else :
+    else:
         weekday = (weekday + tmp % 7) % 7
 
-    return weekdays[weekday-1]
+    return weekdays[weekday - 1]
 
 
 n = int(input())
 for i in range(n):
     m, d = map(int, input().split(' '))
-    print(weekdayChecker(m, d))
+    print(weekdaychecker(m, d))
